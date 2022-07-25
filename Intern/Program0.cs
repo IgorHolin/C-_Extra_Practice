1eb42fefdbc77958b4b982172b4cf40e4e2d88a8
Igor <igor.paluhovicz@gmail.com>
2022-07-25 08:00:46 +0200

    Ex_1_done

--- Intern/Program0.cs
+++ Intern/Program0.cs
@@ -1,11 +1,14 @@
-﻿// 0. Вывести квадрат числа
+﻿// 1. По двум заданным числам проверять является ли первое квадратом второго
 
 Console.Write("Enter the number please: ");
 int num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the number in second power please: ");
+int numPow = Convert.ToInt32(Console.ReadLine());
 
-int Square(int num)
+void IfPowerMatches(int numb, int numbInPow)
 {
-    return num * num;
+    if (numbInPow == numb * numb) Console.Write($"{numbInPow} is {numb} in 2 power");
+    else Console.Write($"{numbInPow} isnt {numb} in 2 power");
 }
 
-Console.WriteLine($"The square of number you've entered is {Square(num)}");
+IfPowerMatches(num,numPow);
