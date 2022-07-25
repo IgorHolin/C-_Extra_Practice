5326b0b6393f0c7c5ee95d7a82def2be80f2e14d
Igor <igor.paluhovicz@gmail.com>
2022-07-25 08:16:04 +0200

    Ex_3_done

--- Intern/Program0.cs
+++ Intern/Program0.cs
@@ -1,15 +1,26 @@
-﻿// 2. Даны два числа. Показать большее и меньшее число
+﻿// 3. По заданному номеру дня недели вывести его название
 
-Console.Write("Enter the first number please: ");
-int x = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter the second number please: ");
-int y = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the number of the day of week: ");
+int day = Convert.ToInt32(Console.ReadLine());
 
-void BiggerSmaller(int fNum, int sNum)
+// проверка на корректный ввод пользователем дня недели
+do  
 {
-    if (fNum > sNum) Console.Write($"{fNum} is bigger, {sNum} is smaller.");
-    else if (fNum < sNum) Console.Write($"{sNum} is bigger, {fNum} is smaller.");
-    else Console.Write("The numbers are even.");
+    Console.Write("Enter the number from 1 to 7:");
+    int x = Convert.ToInt32(Console.ReadLine());
+    day = x;
+} while (day > 7);
+
+
+void WhichDay(int num)
+{
+    if (num == 1) Console.Write("Monday");
+    if (num == 2) Console.Write("Tuesday");
+    if (num == 3) Console.Write("Wednesday");
+    if (num == 4) Console.Write("Thursday");
+    if (num == 5) Console.Write("Friday");
+    if (num == 6) Console.Write("Staurday");
+    if (num == 7) Console.Write("Sunday");
 }
 
-BiggerSmaller(x,y);
+WhichDay(day);
