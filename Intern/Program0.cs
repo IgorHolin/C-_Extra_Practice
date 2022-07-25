63c03ae1b59f4c14b655f2a6790955e5be799739
Igor <igor.paluhovicz@gmail.com>
2022-07-25 07:53:00 +0200

    Ex_0_done

--- Intern/Program0.cs
+++ Intern/Program0.cs
@@ -1,1 +1,11 @@
-﻿
+﻿// 0. Вывести квадрат числа
+
+Console.Write("Enter the number please: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int Square(int num)
+{
+    return num * num;
+}
+
+Console.WriteLine($"The square of number you've entered is {Square(num)}");
