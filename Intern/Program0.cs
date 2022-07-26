ff22cb9ed3855bcb070c7cfbbfd746c8ac1d5da1
Igor <igor.paluhovicz@gmail.com>
2022-07-26 08:46:20 +0200

    Ex_8_done

--- Intern/Program0.cs
+++ Intern/Program0.cs
@@ -1,14 +1,14 @@
-﻿// 7. Показать числа от -N до N
+﻿//  8. Показать четные числа от 1 до N
+
 
 Console.Write("Enter the number please: ");
 int N = Convert.ToInt32(Console.ReadLine());
 
 void Output(int num)
 {
-    for (int i = -num; i <= num; i++)
+    for (int i = 2; i <= N; i+=2)
     {
         Console.Write(i + " ");
     }
 }
-
 Output(N);
