0b53e93ab00bbd74032fdc00b70d311adce39d7a
Igor <igor.paluhovicz@gmail.com>
2022-07-26 08:54:29 +0200

    Ex_10_done

--- Intern/Program0.cs
+++ Intern/Program0.cs
@@ -1,2 +1,13 @@
 ﻿// 10. Показать вторую цифру трёхзначного числа
 
+Console.Write("Enter the 3-digit number please: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+do
+{
+    Console.Write("Number should be more or equal 100 and less or equal 999! Enter once again: ");
+    int numb = Convert.ToInt32(Console.ReadLine());
+    num = numb;
+} while (num < 100 || num > 999);
+
+Console.Write($"The second digit in {num} is {(num/10)%10}");
