074a0374b49b47589d12731a553f984bbdace5df
Igor <igor.paluhovicz@gmail.com>
2022-07-26 08:53:04 +0200

    EX10

--- Intern/Program0.cs
+++ Intern/Program0.cs
@@ -1,14 +1,2 @@
-﻿//  9. Показать последнюю цифру трёхзначного числа
+﻿// 10. Показать вторую цифру трёхзначного числа
 
-
-Console.Write("Enter the 3-digit number please: ");
-int num = Convert.ToInt32(Console.ReadLine());
-
-do
-{
-    Console.Write("Number should be more or equal 100 and less or equal 999! Enter once again: ");
-    int numb = Convert.ToInt32(Console.ReadLine());
-    num = numb;
-} while (num < 100 || num > 999);
-
-Console.Write($"The last digit in {num} is {num%10}");
