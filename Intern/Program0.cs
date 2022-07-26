f7243e478375972f9945ab402e21b30c1ba2af35
Igor <igor.paluhovicz@gmail.com>
2022-07-26 08:33:09 +0200

    Ex_6_done

--- Intern/Program0.cs
+++ Intern/Program0.cs
@@ -1,1 +1,12 @@
-﻿// 5. Написать программу вычисления значения функции y = f(a)
+﻿// 6. Выяснить является ли число чётным
+
+Console.Write("Enter the number please: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+void EvenOrOdd(int number)
+{
+    if (number % 2 == 0) Console.Write($"{number} is even.");
+    else Console.WriteLine($"{number} is odd.");
+}
+
+EvenOrOdd(x);
