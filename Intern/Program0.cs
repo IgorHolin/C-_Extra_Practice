6cae913cd789bb3b2ee2dd36cf7982977b896f26
Igor <igor.paluhovicz@gmail.com>
2022-07-26 08:29:44 +0200

    Ex_5_to_make

--- Intern/Program0.cs
+++ Intern/Program0.cs
@@ -1,16 +1,1 @@
-﻿// 4. Найти максимальное из трех чисел
-
-Console.Write("Enter the number please: ");
-int x = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter the number please: ");
-int y = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter the number please: ");
-int z = Convert.ToInt32(Console.ReadLine());
-
-int TheBiggestNumber(int first, int second, int third)
-{
-    int maxOfTwo = Math.Max(first,second);
-    int anotherMax = Math.Max(maxOfTwo,third);
-    return anotherMax;
-}
-Console.Write(TheBiggestNumber(x,y,z));
+﻿// 5. Написать программу вычисления значения функции y = f(a)
