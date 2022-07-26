13e87021b541e4c079b4e230762ef69f2a085c86
Igor <igor.paluhovicz@gmail.com>
2022-07-26 08:23:51 +0200

    Ex_4_done

--- Intern/Program0.cs
+++ Intern/Program0.cs
@@ -1,26 +1,16 @@
-﻿// 3. По заданному номеру дня недели вывести его название
+﻿// 4. Найти максимальное из трех чисел
 
-Console.Write("Enter the number of the day of week: ");
-int day = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the number please: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the number please: ");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the number please: ");
+int z = Convert.ToInt32(Console.ReadLine());
 
-// проверка на корректный ввод пользователем дня недели
-do  
+int TheBiggestNumber(int first, int second, int third)
 {
-    Console.Write("Enter the number from 1 to 7:");
-    int x = Convert.ToInt32(Console.ReadLine());
-    day = x;
-} while (day > 7);
-
-
-void WhichDay(int num)
-{
-    if (num == 1) Console.Write("Monday");
-    if (num == 2) Console.Write("Tuesday");
-    if (num == 3) Console.Write("Wednesday");
-    if (num == 4) Console.Write("Thursday");
-    if (num == 5) Console.Write("Friday");
-    if (num == 6) Console.Write("Staurday");
-    if (num == 7) Console.Write("Sunday");
+    int maxOfTwo = Math.Max(first,second);
+    int anotherMax = Math.Max(maxOfTwo,third);
+    return anotherMax;
 }
-
-WhichDay(day);
+Console.Write(TheBiggestNumber(x,y,z));
