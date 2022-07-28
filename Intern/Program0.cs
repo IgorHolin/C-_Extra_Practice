1c8e267d4a45bb65a260d701654595908a434777
Igor <igor.paluhovicz@gmail.com>
2022-07-28 08:57:51 +0200

    Ex_12_done

--- Intern/Program0.cs
+++ Intern/Program0.cs
@@ -1,17 +1,13 @@
-﻿// 11. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+﻿// 12. Удалить вторую цифру трёхзначного числа
 
-int NewNumber()
+int n = new Random().Next(100,1000);
+Console.WriteLine($"Number is {n}");
+
+void DeleteDigit(int num)
 {
-    int n = new Random().Next(10, 100);
-    return n;
+    string x = (num/100).ToString();
+    string y = (num%10).ToString();
+    Console.Write($"The number without second digit is {x}{y}");
 }
 
-int number = NewNumber();
-Console.WriteLine($"Your number is {number}");
-
-void BiggerDigit(int num)
-{
-    if (num / 10 > num % 10) Console.Write($"Digit {num / 10} is the biggest");
-    else Console.Write($"Digit {num % 10} is the biggest");
-}
-BiggerDigit(number);
+DeleteDigit(n);
