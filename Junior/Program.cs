3244b6885471e6a9d0f75b08b96ebb8b074fe749
Igor <igor.paluhovicz@gmail.com>
2022-07-29 09:07:12 +0200

    Ex_15_done

--- Junior/Program.cs
+++ Junior/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿// 15. Дано число. Проверить кратно ли оно 7 и 23
+
+Console.Write("Enter the number please: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+bool Divide(int num)
+{
+    return (num % 7 == 0 && num % 23 == 0);
+}
+
+Console.Write(Divide(x));
