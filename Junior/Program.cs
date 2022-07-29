fc71c95d093547ae07dfa04acf91e160714e5c34
Igor <igor.paluhovicz@gmail.com>
2022-07-29 09:25:51 +0200

    Ex_19_done

--- Junior/Program.cs
+++ Junior/Program.cs
@@ -1,3 +1,17 @@
-﻿// 18. Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+﻿// 19. Определить номер четверти плоскости, в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0
 
 
+Console.Write("Enter x plesae. NB, x != 0: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter y plesae. NB, y != 0: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+void WhichQuarter(int a, int b)
+{
+    if (a > 0 & b > 0) Console.Write("First quarter");
+    else if (a < 0 && b > 0) Console.Write("Second quarter");
+    else if (a < 0 && b < 0) Console.Write("Third quarter");
+    else Console.Write("Fourth quarter");
+}
+
+WhichQuarter(x, y);
