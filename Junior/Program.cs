76330bfcc907873d1c883154285f50e2072ec27b
Igor <igor.paluhovicz@gmail.com>
2022-07-30 13:44:19 +0200

    Ex_22_done

--- Junior/Program.cs
+++ Junior/Program.cs
@@ -1,17 +1,47 @@
-﻿// 21. Программа проверяет пятизначное число на палиндромом. 12321
+﻿// 22. Найти расстояние между точками в пространстве 2D/3D
 
-Console.Write("Enter 5-digit number please: ");
-int x = Convert.ToInt32(Console.ReadLine());
 
-while (x < 10000 || x > 99999)
+Console.Write("Enter the dimension you want - 2D or 3D: ");
+string d = Console.ReadLine();
+
+if (d.Contains("2") == true)
 {
-    Console.Write("Number should contain 5 digits. Enter once again please: ");
-    x = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Enter x1: ");
+    double x1 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Enter y1: ");
+    double y1 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Enter x2: ");
+    double x2 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Enter y2: ");
+    double y2 = Convert.ToDouble(Console.ReadLine());
+    Console.Write(TwoDimension(x1, y1, x2, y2));
 }
 
-bool PolyOrNot(int num)
+if (d.Contains("3") == true)
 {
-    return (num / 10000 == num % 10 && (num / 1000) % 10 == (num / 10) % 10);
+    Console.Write("Enter x1: ");
+    double x1 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Enter y1: ");
+    double y1 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Enter z1: ");
+    double z1 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Enter x2: ");
+    double x2 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Enter y2: ");
+    double y2 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Enter z2: ");
+    double z2 = Convert.ToDouble(Console.ReadLine());
+    Console.Write(ThreeDimensions(x1, y1, z1, x2, y2, z2));
 }
 
-Console.Write(PolyOrNot(x));
+
+
+double TwoDimension(double a, double b, double c, double d)
+{
+    return Math.Sqrt(Math.Pow((c - a), 2) + Math.Pow((d - b), 2));
+}
+
+double ThreeDimensions(double a, double b, double c, double d, double e, double f)
+{
+    return Math.Sqrt(Math.Pow((d - a), 2) + Math.Pow((e - b), 2) + Math.Pow((f - c), 2));
+}
