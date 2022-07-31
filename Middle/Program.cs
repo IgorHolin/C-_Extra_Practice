e11cecd6b5b9bdba535af3cdf4c88d46b86f4a2d
Igor <igor.paluhovicz@gmail.com>
2022-07-31 17:37:11 +0200

    Ex_23_done

--- Middle/Program.cs
+++ Middle/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿// 23. Показать таблицу квадратов чисел от 1 до N 
+
+
+Console.Write("Enter the number please: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+void DoublePower(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        Console.WriteLine($"Second power of {i} - {i*i}");
+    }
+}
+
+DoublePower(x);
