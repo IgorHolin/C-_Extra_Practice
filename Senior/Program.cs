cc7350d26486b4c93f98079274575188e9d754c9
Igor <igor.paluhovicz@gmail.com>
2022-08-03 10:19:25 +0200

    Ex_31_done

--- Senior/Program.cs
+++ Senior/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿// 31. Задать массив из 8 элементов и вывести их на экран 
+
+int[] newArr = new int[8];
+
+for (int i = 0; i < newArr.Length; i++)
+{
+    newArr[i] = new Random().Next(1,10);
+    Console.Write(newArr[i] + " ");
+}
