d371b7a7c8faf6ddb23ba12300b14f938c332db4
Igor <igor.paluhovicz@gmail.com>
2022-08-04 09:09:01 +0200

    Ex_35_done

--- Senior/Program.cs
+++ Senior/Program.cs
@@ -1,26 +1,17 @@
-﻿// 34. Написать программу замену элементов массива на противоположные
+﻿// 35. Определить, присутствует ли в заданном массиве, некоторое число 
 
-int[] arr = new int[10];
+Console.Write("Enter the size of array please: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the the number to find: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[size];
 
-void FillPrint(int[] mass)
+for (int i = 0; i < size; i++)
 {
-    for (int i = 0; i < mass.Length; i++)
-    {
-        mass[i] = new Random().Next(-9,10);
-        Console.Write(mass[i] + "\t");
-    }
+    arr[i] = new Random().Next(0, 21);
+    Console.Write(arr[i] + "\t");
 }
-
-FillPrint(arr);
 Console.WriteLine();
 
-void Change(int[] ar)
-{
-    for (int i = 0; i < ar.Length; i++)
-    {
-        ar[i] = -ar[i];
-        Console.Write(ar[i] + "\t");
-    }
-}
-
-Change(arr);
+if (arr.Contains(num) == true) Console.Write($"{num} is in array.");
+else Console.Write($"Array doesnt contain {num}");
