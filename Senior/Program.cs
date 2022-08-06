20c192614f9b34317d0d10632dd9ad4245cfbf65
Igor <igor.paluhovicz@gmail.com>
2022-08-06 10:40:14 +0200

    Ex_39 done

--- Senior/Program.cs
+++ Senior/Program.cs
@@ -1,19 +1,31 @@
-﻿// 37. В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+﻿// 39. Найти произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
 
-int[] arr = new int[123];
-int count = 0;
+Console.Write("Enter the number of digits in array: ");
+int size = Convert.ToInt32(Console.ReadLine());
 
+int[] arr = new int[size];
 
-void FillPrintCount(int[] a)
+void FillPrint(int[] ar)
 {
-    for (int i = 0; i < a.Length; i++)
+    for (int i = 0; i < ar.Length; i++)
     {
-        a[i] = new Random().Next(1, 150);
-        if (a[i] >= 10 && a[i] < 100) count++;
-        Console.Write(a[i] + " ");
+        ar[i] = new Random().Next(100);
+        Console.Write(ar[i] + " ");
     }
 }
-FillPrintCount(arr);
-Console.WriteLine();
 
-Console.Write($"Amount of numbers [10;99] in array is: {count}");
+void PairSum(int[] mas)
+{
+    int count = mas.Length - 1;
+    for (int j = 0; j < mas.Length / 2; j++)
+    {
+        int res = 0;
+        res = mas[j] + mas[count];
+        Console.WriteLine($"Sum of {j+1} pair of nums in array = {res}");
+        count--;
+    }
+}
+
+FillPrint(arr);
+Console.WriteLine();
+PairSum(arr);
