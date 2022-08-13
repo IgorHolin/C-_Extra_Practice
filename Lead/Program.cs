b24ab6b2c870537ce748f686a2bad5b31226a9f5
Igor <igor.paluhovicz@gmail.com>
2022-08-13 09:40:09 +0200

    Ex_41_1_done

--- Lead/Program.cs
+++ Lead/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿// 41. Выяснить являются ли три числа сторонами треугольника 
+
+Console.WriteLine("Enter the side length: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the side length: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the side length: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+bool Triangle(int fside, int sside, int tside)
+{
+    return (a + b > c && a + c > b && b + c > a);
+}
+
+Console.WriteLine(Triangle(a,b,c));
